606c37a3c3422077577033127c2a1d68313700ca
Tomonari Takai <takai01010100@gmail.com>
2021-05-01 23:28:56 +0900

    initial commit with superplate

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { AppProps } from "next/app";
+import "@styles/global.scss";
+import { Provider } from "react-redux";
+import store from "@redux/store";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    return (
+        <Provider store={store}>
+            <Component {...pageProps} />
+        </Provider>
+    );
+}
+
+export default MyApp;
